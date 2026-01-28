0a212e3f9b693df1b11b79285326c844129c5dfd
danil4237 <zdan2007@gmail.com>
2026-01-28 17:49:58 +0300

    Name

--- backend/program.cs
+++ backend/program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello!");
+Console.WriteLine("Введите ваше имя");
+string name = Console.ReadLine();
 Console.WriteLine("Danil Zhadaev");
 Console.WriteLine(DateTime.Now);
