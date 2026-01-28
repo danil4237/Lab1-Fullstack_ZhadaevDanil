8b0ee4daeefaa27d5fb3c23f9b204a459752d6a5
danil4237 <zdan2007@gmail.com>
2026-01-28 17:52:37 +0300

    dayofweek

--- backend/program.cs
+++ backend/program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Введите ваше имя");
 string name = Console.ReadLine();
-Console.WriteLine("Danil Zhadaev");
-Console.WriteLine(DateTime.Now);
+Console.WriteLine($"привет, {name}!");
+Console.WriteLine(dayOfWeek);
+Console.WriteLine("Хотите продолжить?");
+string answer = Console.ReadLine();
