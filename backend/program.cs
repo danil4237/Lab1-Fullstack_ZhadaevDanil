b65689e451dc6bc79c85f341793335e7e34cd7d6
danil4237 <zdan2007@gmail.com>
2026-01-28 18:00:58 +0300

    next

--- backend/program.cs
+++ backend/program.cs
@@ -2,6 +2,16 @@
 Console.WriteLine("Введите ваше имя");
 string name = Console.ReadLine();
 Console.WriteLine($"привет, {name}!");
+string dayOfWeek =  DateTime.Now.ToString("dddd", new System.Globalization.CultureInfo("ru-RU"));
 Console.WriteLine(dayOfWeek);
 Console.WriteLine("Хотите продолжить?");
 string answer = Console.ReadLine();
+
+if (answer.ToLower() == "yes")
+{
+    Console.WriteLine("Продолжаем");
+}
+else
+{
+    Console.WriteLine("Завершаем");
+}
